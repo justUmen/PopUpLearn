12b1fe598f988b2b83060d79855c552bfe88e73f
justumen <me@justumen.org>
2019-01-09 04:31:34 +0700

    ../..

--- popup.php
+++ popup.php
@@ -231,10 +231,7 @@ function close_popup_bad(){
 }
 close_popup_bad();
 
-var audio_1 = new Audio();
-audio_1.type = 'audio/mp3';
 //audio_1.onerror = function(){ document.getElementById('audio_1').style.display="none";};
-audio_1.src="http://127.0.0.1:9092/soundDB/<?php echo $LANGUAGE_TAG_1; ?>/<?php echo $LEFT; ?>.mp3"
 
 var audio_2 = new Audio();
 audio_2.type = 'audio/mp3';
@@ -242,6 +239,9 @@ audio_2.type = 'audio/mp3';
 audio_2.src="http://127.0.0.1:9092/soundDB/<?php echo $LANGUAGE_TAG_2; ?>/<?php echo $RIGHT; ?>.mp3"
 
 function audio_play_1() {
+	var audio_1 = new Audio();
+	audio_1.type = 'audio/mp3';
+	audio_1.src="http://127.0.0.1:9092/soundDB/<?php echo $LANGUAGE_TAG_1; ?>/<?php echo $LEFT; ?>.mp3"
   var playPromise = audio_1.play();
   if (playPromise !== undefined) {
       playPromise.then(function () {
