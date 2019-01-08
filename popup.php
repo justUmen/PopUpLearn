b5044ac95975d40adcc6859a9745e47b730dbd81
justumen <me@justumen.org>
2019-01-09 03:49:00 +0700

    ../..

--- popup.php
+++ popup.php
@@ -241,17 +241,10 @@ if(url==false){
     //url exists
 		document.getElementById('audio_1').style.display="none";
 }
-function CheckUrl(url)
-{
-     if (window.XMLHttpRequest)
-    {// code for IE7+, Firefox, Chrome, Opera, Safari
+function CheckUrl(url){
     var http=new XMLHttpRequest();
-     }
-    else
-    {// code for IE6, IE5
-   var http=new ActiveXObject("Microsoft.XMLHTTP");
-     }
     http.open('HEAD', url, false);
+		http.onerror = function(){ document.getElementById('audio_1').style.display="none";};
     http.send();
     return http.status!=404;
 }
