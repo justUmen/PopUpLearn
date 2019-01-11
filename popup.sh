d31a1a217259fe776f8aa7ead13e801c734dd492
justumen <me@justumen.org>
2019-01-11 19:08:11 +0700

    ../..

--- popup.sh
+++ popup.sh
@@ -255,7 +255,7 @@ function ⬚⬚_📃_main(){ 🔧 $FUNCNAME $@
 			"jpRO") L2="japanese[romaji]" ;;
 			"RO") L2="japanese (romaji only)" ;;
 		esac
-		echo " => $COLOR_PERCENT $PERCENT% done ($NB_GOOD / $NB_LINES) $ENDO $COLOR_SELECTION $L1 / $L2 $ENDO|" >> $HOME/.PopUpLearn/tmp/color_menu.list
+		echo " => $COLOR_PERCENT $PERCENT% done ($NB_GOOD / $NB_LINES) $ENDO| $COLOR_SELECTION $L1 / $L2 $ENDO" >> $HOME/.PopUpLearn/tmp/color_menu.list
 	done
 	echo -e "`cat $HOME/.PopUpLearn/tmp/color_menu.list 2> /dev/null`" | column -t -s '|'
 	echo -e "$COLOR_SELECTION g) $ENDO GameScript Quizzes [for `cat ~/.GameScript/username`]"
