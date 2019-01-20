ff27050cce1dd96dada02219a06886cedb86f784
justumen <me@justumen.org>
2019-01-20 22:27:45 +0700

    ../..

--- popup.sh
+++ popup.sh
@@ -901,11 +901,13 @@ function ⬚⬚⬚⬚_📃_session(){ 🔧 $FUNCNAME $@
 					DAYS_AGO_GOOD_LINE2=`cat $HOME/.PopUpLearn/tmp/line2_good_answers_days.tmp | head -n 1`
 					DAYS_AGO_BAD_LINE2=`cat $HOME/.PopUpLearn/tmp/line2_bad_answers_days.tmp | head -n 1`
 					LEVEL_LINE2=3
-					if [ $DAYS_AGO_GOOD_LINE2 -lt $DAYS_AGO_BAD_LINE2 ]; then
-						echo -e "PINK_1 $line2" >> $HOME/.PopUpLearn/tmp/colors_session_$ARG.tmp
-					else
-						if [ $DAYS_AGO_GOOD_LINE2 -gt $LEVEL_LINE2 ]; then
-							echo -e "PINK_2 $line2" >> $HOME/.PopUpLearn/tmp/colors_session_$ARG.tmp
+					if [ "$DAYS_AGO_GOOD_LINE2" ] && [ "$DAYS_AGO_BAD_LINE2" ]; then
+						if [ $DAYS_AGO_GOOD_LINE2 -lt $DAYS_AGO_BAD_LINE2 ]; then
+							echo -e "PINK_1 $line2" >> $HOME/.PopUpLearn/tmp/colors_session_$ARG.tmp
+						else
+							if [ $DAYS_AGO_GOOD_LINE2 -gt $LEVEL_LINE2 ]; then
+								echo -e "PINK_2 $line2" >> $HOME/.PopUpLearn/tmp/colors_session_$ARG.tmp
+							fi
 						fi
 					fi
 					LINE2_LEVEL="0" #3, 6, 14, 28...
