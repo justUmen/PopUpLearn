80ddd103d14cda1aaad31af9374a01ad81cfb91e
justumen <me@justumen.org>
2019-01-21 02:41:31 +0700

    ../..

--- popup.sh
+++ popup.sh
@@ -910,9 +910,9 @@ function ⬚⬚⬚⬚_📃_session(){ 🔧 $FUNCNAME $@
 						else
 							LINE2_A=`expr $DAYS_AGO_GOOD_LINE2 - $DAYS_AGO_BAD_LINE2`
 							if [ $DAYS_AGO_GOOD_LINE2 -lt $LEVEL_LINE2 ]; then
-								echo -e "\\\nPINK_2 $line2 ($DAYS_AGO_GOOD_LINE2 , $DAYS_AGO_BAD_LINE2) TODAY=$TODAY" >> $HOME/.PopUpLearn/tmp/colors_session_$ARG.tmp
-							else
 								echo -e "\\\nNOTYYY $line2 ($DAYS_AGO_GOOD_LINE2 , $DAYS_AGO_BAD_LINE2) TODAY=$TODAY" >> $HOME/.PopUpLearn/tmp/colors_session_$ARG.tmp
+							else
+								echo -e "\\\nPINK_2 $line2 ($DAYS_AGO_GOOD_LINE2 , $DAYS_AGO_BAD_LINE2) TODAY=$TODAY" >> $HOME/.PopUpLearn/tmp/colors_session_$ARG.tmp
 							fi
 						fi
 					fi
