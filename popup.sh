a9990b207eaae145b896ef7a4fac2d11315e6c03
justumen <me@justumen.org>
2019-01-22 01:41:54 +0700

    ../..

--- popup.sh
+++ popup.sh
@@ -902,10 +902,10 @@ function ⬚⬚⬚⬚_📃_session(){ 🔧 $FUNCNAME $@
 					DAYS_AGO_BAD_LINE2=`cat $HOME/.PopUpLearn/tmp/line2_bad_answers_days.tmp | tail -n 1`
 					if [ "$DAYS_AGO_GOOD_LINE2" ]; then
 						if [ ! "$DAYS_AGO_BAD_LINE2" ];then
-							DAYS_AGO_BAD_LINE2=99999
+							DAYS_AGO_BAD_LINE2=`cat $HOME/.PopUpLearn/tmp/line2_good_answers_days.tmp | head -n 1` #BAD is the date of the first good answer ??? Strange but maybe working...
 						fi
 						if [ $DAYS_AGO_GOOD_LINE2 -gt 3 ]; then
-							LINE2_A=`expr $DAYS_AGO_GOOD_LINE2 - $DAYS_AGO_BAD_LINE2`
+							LINE2_A=`expr $DAYS_AGO_BAD_LINE2 - $DAYS_AGO_GOOD_LINE2`
 							if [ $DAYS_AGO_GOOD_LINE2 -gt $DAYS_AGO_BAD_LINE2 ] || [ $DAYS_AGO_GOOD_LINE2 -gt $LINE2_A ]; then
 								echo -e "\\\nPINK $line2 ($DAYS_AGO_GOOD_LINE2 , $DAYS_AGO_BAD_LINE2) TODAY=$TODAY" >> $HOME/.PopUpLearn/tmp/colors_session_$ARG.tmp
 							else
