5a44be6ead4f944e00f197fa7b1ca9f3f857cd1a
umen <u-men@outlook.com>
2019-02-17 21:55:13 +0100

    ../..

--- read_logs.php
+++ read_logs.php
@@ -681,7 +681,7 @@ for($i=18;$i!=0;$i--){
 
   $session_lines_level=array(); $fn = fopen("$PATH/answer.level", 'r');// or die("fail to open file session_lines_level - session $i");
   while($row = fgets($fn)) { array_push($session_lines_level, explode('€', $row)); }
-  // echo '<pre>'; print_r($session_lines_level); echo '</pre>';
+  echo '<pre>'; print_r($session_lines_level); echo '</pre>';
 
   // ██████  ██ ███████ ██████  ██       █████  ██    ██
   // ██   ██ ██ ██      ██   ██ ██      ██   ██  ██  ██
