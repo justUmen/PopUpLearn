3789756b0660e1f4b1cc6519ed35cd8df5774886
umen <u-men@outlook.com>
2019-02-21 15:06:33 +0100

    ../..

--- read_logs.php
+++ read_logs.php
@@ -613,7 +613,7 @@ $DELAY_DAYS_ERRORS=$TODAY-$DELAY; //If error was long time ago, ignore it
 ?>
 
 <h3 style="font-family:Monospace;color:white;">Pul file :
-<select onchange="window.location='read_logs.php?DELAY=<?php echo $DELAY_DAYS_ERRORS; ?>&PUL=' + document.getElementById('select_pul').value;" id="select_pul" class="styled-select">
+<select onchange="window.location='read_logs.php?DELAY=<?php echo $DELAY; ?>&PUL=' + document.getElementById('select_pul').value;" id="select_pul" class="styled-select">
 <?php
 rsearch('logs/', "/session_1/session_content.pul",$PUL);
 ?>
