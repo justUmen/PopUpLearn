19b93c609717fe54b93b8cd1c0d6a9c351ce8867
umen <u-men@outlook.com>
2019-02-23 17:53:30 +0100

    ../..

--- read_logs.php
+++ read_logs.php
@@ -769,7 +769,7 @@ for($i=$PUL_NB_SESSIONS;$i!=0;$i--){
     }
 
     //FIND WHEN WAS LAST GOOD
-    $LAST_GOOD_PINK=0;
+    $LAST_USEFUL_GOOD=0;
     $THE_GOOD_GRID="";
     foreach($good_dates_lines as $good_line){
       if($good_line[0]=="$line"){
@@ -786,7 +786,7 @@ for($i=$PUL_NB_SESSIONS;$i!=0;$i--){
         // echo " --- $GOOD>$TODAY-$LAST_GOOD_PINK+$LEVEL --- ";
         if($LAST_GOOD==0){
           $LAST_GOOD=$TODAY-$GOOD;
-          if($GOOD<$TODAY + $LAST_BAD){
+          if($GOOD>$TODAY + $LAST_BAD){
             $LAST_USEFUL_GOOD=$TODAY-$GOOD;
           }
         }
@@ -794,7 +794,7 @@ for($i=$PUL_NB_SESSIONS;$i!=0;$i--){
           // echo "--- $LAST_GOOD_PINK<$GOOD && $GOOD<$TODAY-$LAST_BAD && $GOOD>$TODAY-$LAST_GOOD_PINK-$LEVEL ---<br>";
           if($LAST_GOOD<$GOOD){
             $LAST_GOOD=$TODAY-$GOOD;
-            if($GOOD<$TODAY + $LAST_BAD){
+            if($GOOD>$TODAY + $LAST_BAD){
               $LAST_USEFUL_GOOD=$TODAY-$GOOD;
             }
           }
