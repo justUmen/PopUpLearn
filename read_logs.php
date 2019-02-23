5e03d7b9a27d736c871cf41953295e6296e3c229
umen <u-men@outlook.com>
2019-02-23 22:58:10 +0100

    ../..

--- read_logs.php
+++ read_logs.php
@@ -831,7 +831,7 @@ for($i=$PUL_NB_SESSIONS;$i!=0;$i--){
     $WHITE_BLUE_PINK="white";
     $NOT_PINK="";
     if($LAST_USEFUL_GOOD >= $LEVEL){$WHITE_BLUE_PINK="magenta";$CLASS="";} //Disable gray line (hide) if pink. or equal ???
-    else{$TMP=$LEVEL - $LAST_USEFUL_GOOD;$NOT_PINK="Pink $TMP";}
+    else{$TMP=$LEVEL - $LAST_USEFUL_GOOD;$NOT_PINK="Pink $TMP ($LEVEL - $LAST_USEFUL_GOOD)";}
 
     //$MOST_RECENT_BAD : $LAST_USEFUL_GOOD --
     $THE_GRID.="<div class='grid-item$CLASS'>$NOT_PINK";
