c9c4a228323a0ffc3dee2a33378fd0544e70f765
umen <u-men@outlook.com>
2019-02-23 22:36:02 +0100

    ../..

--- read_logs.php
+++ read_logs.php
@@ -831,7 +831,7 @@ for($i=$PUL_NB_SESSIONS;$i!=0;$i--){
     $WHITE_BLUE_PINK="white";
     if($LAST_USEFUL_GOOD > $LEVEL * 2){$WHITE_BLUE_PINK="magenta";$CLASS="";} //Disable gray line (hide) if pink.
 
-    $THE_GRID.="<div class='grid-item$CLASS'>$MOST_RECENT_BAD : $LAST_USEFUL_GOOD -- {$LEVEL * 2}";
+    $THE_GRID.="<div class='grid-item$CLASS'>$MOST_RECENT_BAD : $LAST_USEFUL_GOOD -- " . $LEVEL * 2;
     switch ($LEVEL) {
       case 3: $level="<span class='tooltip_right' style='color:$WHITE_BLUE_PINK;'>⚀<span class='tooltiptext_right'>3+ days streak</span></span>";break;
       case 6: $level="<span class='tooltip_right' style='color:$WHITE_BLUE_PINK;'>⚀⚁<span class='tooltiptext_right'>6+ days streak</span></span>";break;
